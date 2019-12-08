bb91916937b172a2867375c63a6b4d2af838f6e1
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-12-08 23:18:51 +0530

    removed database consoles

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ const url = process.env.DB_URL || "mongodb://localhost:27017/ecomm";
 const port = process.env.PORT || 3000;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-const db = mongoose.connection;
-db.once("open", () => console.log("Connected to database"));
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
 app.use(logger('dev'));
 app.use(parsedBody.json());
