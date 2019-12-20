aa0365ed15e333d5b3c863a0f09f0b89217b1115
Sunny Prakash <sunny.prakashgm@gmail.com>
2019-12-20 22:55:03 +0530

    modified index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const orderRoutes = require('./routes/order');
 const url = process.env.DB_URL || "mongodb://localhost:27017/ecomm";
 const port = process.env.PORT || 3000;
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log('DB error', err));
 
 app.use(logger('dev'));
 app.use(parsedBody.json());
